922aad6110e9c16c37a6ee2d9d3627b8e8754b98
Antonio Rafael Cedillo Rodríguez <A01656823@tec.mx>
2024-09-23 19:44:20 -0600

    Character select terminado
    Se  agrego una escena nueva que consiste en la selección de personajes. Cada personaje tiene su botón, con la cual al presionarlo cambia el modelo, nombre e información del personaje respectivo.
    
    Se agrego un botón de inicio, pero falta conectarlo con la funcionalidad.

--- Shadow Hunters of Willow Creek/Assets/Scripts/CharacterSelect/CharacterSelect.cs
+++ Shadow Hunters of Willow Creek/Assets/Scripts/CharacterSelect/CharacterSelect.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSelect : MonoBehaviour
 {
@@ -8,7 +10,24 @@ public class CharacterSelect : MonoBehaviour
     private List<GameObject> models;
 
     //default index of list models
-    private int selection = 0; 
+    private int selection = 0;
+
+
+    //To change the character name in character select 
+    [SerializeField]
+    private TMP_Text _character;
+
+    //List of text
+    private List<string> names = new List<string> { "Fenrir Ha’it", "Lucius Blackwood", "Navi’srah de Naturah", "Ambrose Morningbane" };
+
+    //Sprites
+    public Sprite FenrirInfo;
+    public Sprite LuciusInfo;
+    public Sprite NaviInfo;
+    public Sprite AmbroseInfo;
+
+    public Image CharacterInfo; 
+
     void Start()
     {
         models = new List<GameObject>();
@@ -17,14 +36,69 @@ public class CharacterSelect : MonoBehaviour
         {
             models.Add(t.gameObject);
             t.gameObject.SetActive(false);
+           
 
         }
         models[selection].SetActive(true);
     }
 
+    public void Select(int index)
+    {
+        if (index == selection)
+        {
+            Debug.Log("The index and the selection you made is the same.");
+            return;
+
+        }
+            
+
+        if (index < 0 || index >= models.Count)
+        {
+            Debug.Log("The index is lower than 0 or is higher than the amount of models");
+            return;
+        }    
+           
+
+        models[selection].SetActive(false);
+        selection = index;
+        
+        //Selección de personaje realizada
+        models[selection].SetActive(true);
+
+        //Cambio de personaje 
+        _character.text = names[selection];
+
+        //Cambio de sprite 
+        if (selection == 0)
+        {
+            CharacterInfo.sprite = FenrirInfo;
+        }
+
+        if(selection == 1)
+        {
+            CharacterInfo.sprite = LuciusInfo;
+        }
+
+        if (selection == 2)
+        {
+            CharacterInfo.sprite = NaviInfo;
+        }
+
+        if (selection == 3)
+        {
+            CharacterInfo.sprite = AmbroseInfo;
+        }
+
+
+
+
+
+    }
+
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+            Select(2);
     }
 }
