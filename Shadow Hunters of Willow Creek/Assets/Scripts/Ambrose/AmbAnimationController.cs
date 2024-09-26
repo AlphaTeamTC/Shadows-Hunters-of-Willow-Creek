a8d03b7351b7b92e846f277f8e5e8e492970eebf
Klaus Cedillo <78632803+H0PE467@users.noreply.github.com>
2024-09-25 18:05:46 -0600

    Lifebars Added
    - Lifebar Added
    - Energybar Added
    - Dying animation
    - Hurt animation
    - CC Bug Fixed
    - Documentation of scripts done

--- Shadow Hunters of Willow Creek/Assets/Scripts/Ambrose/AmbAnimationController.cs
+++ Shadow Hunters of Willow Creek/Assets/Scripts/Ambrose/AmbAnimationController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class AmbAnimationController : MonoBehaviour
@@ -8,7 +6,7 @@ public class AmbAnimationController : MonoBehaviour
     private Animator animator;
     [SerializeField] private GameObject ambrose;
 
-    // Start is called before the first frame update
+    // Initialize the animator and initial values
     void Start()
     {
         animator = ambrose.GetComponent<Animator>();
@@ -16,36 +14,53 @@ public class AmbAnimationController : MonoBehaviour
         animator.SetFloat("ySpeed", 0);
     }
 
+    // Set the walking/running animations
     public void setAnimation(float speed)
     {
         animator.SetFloat("ySpeed", speed);
     }
 
+    // Set the falling animation
     public void Falling()
     {
         animator.SetBool("isFalling", true);
     }
 
+    // Set the landing animation when ends falling
     public void FallToStandAnimation()
     {
         animator.SetBool("isLanding", true);
     }
 
+    // Set the crouching animation
     public void Crouching()
     {
         animator.SetBool("isCrouching", true);
 
     }
 
+    // Set the crouching animation false
     public void Standing()
     {
         animator.SetBool("isCrouching", false);
 
     }
+
+    // Set the jumping animation
     public void Jump()
     {
         animator.SetBool("isJumping", true);
 
+    }
+
+    // Set the damage reaction animation
+    public void TakeDamage(){
+        animator.Play("TakeDamage");
+    }
+
+    // Set the dying animation
+    public void Die(){
+        animator.SetBool("isDead", true);
     }
 }
 
