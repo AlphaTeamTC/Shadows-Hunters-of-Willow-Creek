1be08c290b43fc3f6680f4602d3ce557f01ea314
Antonio Rafael Cedillo Rodríguez <116749018+AntonioCedilloA01656823@users.noreply.github.com>
2024-11-11 00:18:20 -0600

    Options Menu finished
    Basic structure of options menu finished, needs to be checked and optimized with the Photon addons.

--- Shadow Hunters of Willow Creek/Assets/Scripts/UI/Settings/OptionsMenu.cs
+++ Shadow Hunters of Willow Creek/Assets/Scripts/UI/Settings/OptionsMenu.cs
@@ -11,13 +11,25 @@ namespace UI.Settings
     /// </summary>
     public class OptionsMenu : MonoBehaviour
     {
-        [Header("Volume")] 
-        [SerializeField] Slider volumeSlider;
+        [Header("Volume")] [SerializeField] Slider volumeSlider;
         [SerializeField] private AudioMixer masterMixer;
-        
-        [Header("Name")]
-        [SerializeField] TMP_InputField nameField;
-        
+
+        [Header("Name")] [SerializeField] TMP_InputField nameField;
+
+        [Header("Resolution")] [SerializeField]
+        TMP_Dropdown resolution;
+
+
+        //Class 
+        public PlayerSettings settings = new PlayerSettings();
+
+        //JSON STRING
+        public string jsonSettingsString;
+
+        //Settings
+        [Header("Settings (JSON)")] [SerializeField]
+        SettingsManager settingsManager;
+
         /// <summary>
         /// Initial settings for game 
         /// </summary>
@@ -27,23 +39,21 @@ namespace UI.Settings
             SetVolume(25);
         }
 
-        void Update()
-        {
-        }
 
 
         // Methods for Volume ------
-        
+
         /// <summary>
         /// Method to set the volume of the audio mixer with code/direct float value
         /// </summary>
         /// <param name="_value"></param>
         public void SetVolume(float _value)
         {
-            if(_value < 1)
+            if (_value < 1)
             {
-                _value = 0.001f; 
+                _value = 0.001f;
             }
+
             RefreshSlider(_value);
             masterMixer.SetFloat("MasterVolume", Mathf.Log10(_value / 100) * 20f);
         }
@@ -73,19 +83,25 @@ namespace UI.Settings
         {
             return volumeSlider.value;
         }
-        
-        
+
+
         //Methods for Resolution --------
         public void SetResolution(int resolutionIndex)
         {
             switch (resolutionIndex)
             {
-                case 0: Screen.SetResolution(640,480,FullScreenMode.Windowed); break;
-                case 1: Screen.SetResolution(1200, 720, FullScreenMode.Windowed); break;
-                case 2: Screen.SetResolution(1920, 1080, FullScreenMode.Windowed); break;
+                case 0:
+                    Screen.SetResolution(640, 480, FullScreenMode.Windowed);
+                    break;
+                case 1:
+                    Screen.SetResolution(1200, 720, FullScreenMode.Windowed);
+                    break;
+                case 2:
+                    Screen.SetResolution(1920, 1080, FullScreenMode.Windowed);
+                    break;
             }
-        } 
-        
+        }
+
         //Methods for Name ------
         public void setPlayerName(string name)
         {
@@ -93,16 +109,78 @@ namespace UI.Settings
             {
                 Debug.LogError("Name can't be empty");
             }
+
             //Warn player that the name has been saved
             //Debug.LogWarning($"Player name set to: {name}."); - For when multiplayer is implemented
             Debug.LogWarning($"Player name set to: {nameField.text}.");
 
-            
+
             //Save the player name
             //Debug.Log($"Name saved: {name}."); - For when multiplayer is implemented
-            Debug.Log($"Name saved: {nameField.text}."); 
+            Debug.Log($"Name saved: {nameField.text}.");
         }
-    }
+
+
+        //Methods for setting buttons ------
+        /// <summary>
+        /// MEthod to reset the settings
+        /// Volume: 20/100
+        /// Resolution: 1200 x 720
+        /// Name: Empty
+        /// </summary>
+        public void onResetSettings()
+        {
+            //Volume
+            SetVolume(20);
+            //Resolution
+            SetResolution(2);
+            //Name
+            setPlayerName("");
+        }
+
+        /// <summary>
+        /// Save the settings into the JSON 
+        /// </summary>
+        public void onSaveSettings()
+        {
+            settings.volumeSettings = volumeSlider.value;
+            settings.playerName = nameField.text;
+            settings.resolutionIndex = resolution.value;
+            settingsManager.saveSettings(settings);
+        }
+
+        /// <summary>
+        /// Discard settings u put into the most recent ones, if not resets it.
+        /// </summary>
+        public void onDiscardSettings()
+        {
+            if (jsonSettingsString != null)
+            {
+                settings = settingsManager.loadSettings();
+
+                SetVolume(settings.volumeSettings);
+                SetResolution(settings.resolutionIndex);
+                setPlayerName(settings.playerName);
+
+            }
+
+            else
+            {
+
+                //Volume
+                SetVolume(20);
+                //Resolution
+                SetResolution(2);
+                //Name
+                setPlayerName("");
+            }
+        }
+
+        public void onExit()
+        {
+            Debug.Log("Exiting  options menu");
+        }
+}
 }
 
 
